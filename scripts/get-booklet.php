ea809936167812e7cbf1e4ffb842cd7368728190
classe34-boolean <classe34.boolean@gmail.com>
2021-07-08 09:43:34 +0200

    libretto  in versione Prepared Statement

--- scripts/get-booklet.php
+++ scripts/get-booklet.php
@@ -2,7 +2,23 @@
 require_once __DIR__ . "/database.php";
 
 // possibile ottimizzazione: sostituire query() con Prepared Statement
-$sql = "SELECT 
+// $sql = "SELECT 
+//     `students`.`name`, `students`.`surname`,
+//     `students`.`registration_number`, 
+//     `courses`.`id`, 
+//     `courses`.`name` AS `course_name`,
+//     `exams`.`date`, 
+//     `exam_student`.`vote`
+//     FROM `exam_student`
+//     JOIN `students` ON `exam_student`.`student_id` = `students`.`id`
+//     JOIN `exams` ON `exam_student`.`exam_id` = `exams`.`id`
+//     JOIN `courses` ON `exams`.`course_id` = `courses`.`id`
+//     WHERE `students`.`id` = ".$_GET["id"] . "
+//     AND `exam_student`.`vote` >= 18";
+// $result = $conn->query($sql);
+
+// versione Prepared Statement
+$stmt = $conn->prepare("SELECT 
     `students`.`name`, `students`.`surname`,
     `students`.`registration_number`, 
     `courses`.`id`, 
@@ -13,9 +29,12 @@ $sql = "SELECT
     JOIN `students` ON `exam_student`.`student_id` = `students`.`id`
     JOIN `exams` ON `exam_student`.`exam_id` = `exams`.`id`
     JOIN `courses` ON `exams`.`course_id` = `courses`.`id`
-    WHERE `students`.`id` = ".$_GET["id"] . "
-    AND `exam_student`.`vote` >= 18";
-$result = $conn->query($sql);
+    WHERE `students`.`id` = ?
+    AND `exam_student`.`vote` >= 18");
+
+$stmt->bind_param("s", $_GET["id"]);    	
+$stmt->execute();
+$result = $stmt->get_result();
 
 $exams = [];
 while($row = $result->fetch_assoc()) {
